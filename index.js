8cb3e3ad125c1536436c386c1edbe5c16ce4135b
Keittah Sewe <kseweo@gmail.com>
2022-12-05 11:36:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+const employee = {
+    name: "Sewe", 
+    streetAddress: "Kungu",
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    return{
+        ...obj,
+        [key]: value,
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key){
+    const deletedEmployee = {...obj};
+    delete deletedEmployee[key];
+    return deletedEmployee; 
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key];
+    return obj;
+}
